2966f200e492848ef6425c7f4dd70a94d3937648
fish-in-a-puddle <egtoms314@gmail.com>
2026-09-12 18:20:42 -0300

    Upload code files

---
+++ script.js
@@ -0,0 +1,235 @@
+function share() {
+    navigator.clipboard.writeText('URL to come :)');
+}
+
+function generateImage() {
+let pickImage = Math.floor(Math.random()*54);
+switch (pickImage) {
+    //DO NOT ADD THIRTY FOUR - IT DOES NOT EXIST
+    //THIRTY FIVE IS ALREADY USED
+    case 0:
+        document.getElementById("picture").src = "images/thirtyfive.jpg";
+        document.getElementById("caption").textContent = "Apollo's 4th birthday. He got a cookie.";
+        break;
+    case 1:
+        document.getElementById("picture").src = "images/one.jpg";
+        document.getElementById("caption").textContent = "Sleepy puppy watching the neighborhood.";
+        break;
+    case 2: 
+        document.getElementById("picture").src = "images/two.jpg";
+        document.getElementById("caption").textContent = "Car ride to the park";
+        break;
+    case 3:
+        document.getElementById("picture").src = "images/three.jpg";
+        document.getElementById("caption").textContent = "He needs lots of pets to acheive peak performance.";
+        break;
+    case 4:
+        document.getElementById("picture").src = "images/four.jpg";
+        document.getElementById("caption").textContent = "My dog may or may not be a cat. He flops over immediately in patches of sun.";
+        break;
+    case 5:
+        document.getElementById("picture").src = "images/five.jpg";
+        document.getElementById("caption").textContent = "Neighborhood watch is hard at work.";
+        break;
+    case 6:
+        document.getElementById("picture").src = "images/six.jpg";
+        document.getElementById("caption").textContent = "The grooming lady got Covid so his face ended up getting very fluffy while she recovered.";
+        break;
+    case 7:
+        document.getElementById("picture").src = "images/seven.jpg";
+        document.getElementById("caption").textContent = "Apollo likes to curl up into a nice little donut with his star.";
+        break;
+    case 8:
+        document.getElementById("picture").src = "images/eight.jpg";
+        document.getElementById("caption").textContent = "Apollo has a couple different bow ties for special occasions, like the family picnic that he's at.";
+        break;
+    case 9:
+        document.getElementById("picture").src = "images/nine.jpg";
+        document.getElementById("caption").textContent = "Sitting very patiently in the grass while dumb humanss talk about dumb human stuff.";
+        break;
+    case 10:
+        document.getElementById("picture").src = "images/ten.jpg";
+        document.getElementById("caption").textContent = "He's enjoying the sunshine";
+        break;
+    case 11:
+        document.getElementById("picture").src = "images/eleven.jpg";
+        document.getElementById("caption").textContent = "Puppy on the back of the couch";
+        break;
+    case 12:
+        document.getElementById("picture").src = "images/twelve.jpg";
+        document.getElementById("caption").textContent = "He's curled up into a donut";
+        break;
+    case 13:
+        document.getElementById("picture").src = "images/thirteen.jpg";
+        document.getElementById("caption").textContent = "Must meet daily quota of belly rubs or else";
+        break;
+    case 14:
+        document.getElementById("picture").src = "images/fourteen.jpg";
+        document.getElementById("caption").textContent = "Gotta scritch the itchy ear";
+        break;
+    case 15:
+        document.getElementById("picture").src = "images/fifteen.jpg";
+        document.getElementById("caption").textContent = "XTREME CLOSEUP";
+        break;
+    case 16:
+        document.getElementById("picture").src = "images/sixteen.jpg";
+        document.getElementById("caption").textContent = "Look at this model citizen. You would never guess that he was a sock criminal in his early days.";
+        break;
+    case 17:
+        document.getElementById("picture").src = "images/seventeen.jpg";
+        document.getElementById("caption").textContent = "Sometimes you have to put the puppy on the table. That's just how it is.";
+        break;
+    case 18:
+        document.getElementById("picture").src = "images/eighteen.jpg";
+        document.getElementById("caption").textContent = "Comfy puppy on the couch";
+        break;
+    case 19:
+        document.getElementById("picture").src = "images/nineteen.jpg";
+        document.getElementById("caption").textContent = "Sleepy boy all sprawled out";
+        break;
+    case 20:
+        document.getElementById("picture").src = "images/twenty.jpg";
+        document.getElementById("caption").textContent = "I wanted to take a picture of the big yawn but I was too late.";
+        break;
+    case 21:
+        document.getElementById("picture").src = "images/twentyone.jpg";
+        document.getElementById("caption").textContent = "Donut baby in his fancy bed";
+        break;
+    case 22:
+        document.getElementById("picture").src = "images/twentytwo.jpg";
+        document.getElementById("caption").textContent = "Flopped on the nice sunny couch";
+        break;
+    case 23:
+        document.getElementById("picture").src = "images/twentythree.jpg";
+        document.getElementById("caption").textContent = "There's a balcony at my grandparents' apartment and Apollo loves to sit out there and watch the world.";
+        break;
+    case 24:
+        document.getElementById("picture").src = "images/twentyfour.jpg";
+        document.getElementById("caption").textContent = "Loaf of dog";
+        break;
+    case 25:
+        document.getElementById("picture").src = "images/twentyfive.jpg";
+        document.getElementById("caption").textContent = "Loaf of dog";
+        break;
+    case 26:
+        document.getElementById("picture").src = "images/twentysix.jpg";
+        document.getElementById("caption").textContent = "Incredible angle of him hanging out on the back of the couch";
+        break;
+    case 27:
+        document.getElementById("picture").src = "images/twentyseven.jpg";
+        document.getElementById("caption").textContent = "In the car on his 5th birthday! We went downtown for a little adventure and got him a cookie.";
+        break;
+    case 28:
+        document.getElementById("picture").src = "images/twentyeight.jpg";
+        document.getElementById("caption").textContent = "He's all tired out after his birthday";
+        break;
+    case 29:
+        document.getElementById("picture").src = "images/twentynine.jpg";
+        document.getElementById("caption").textContent = "The first picture we ever saw of Apollo - it was taken when he was 3 weeks old. It's hard to see the scale, but judging by how small he was when he came home five weeks later, he was probably around the size of a honeycrisp apple.";
+        break;
+    case 30:
+        document.getElementById("picture").src = "images/thirty.jpg";
+        document.getElementById("caption").textContent = "The lady we got him from was also a professional photographer so she took some really cute pictures of Apollo and his siblings.";
+        break;
+    case 31:
+        document.getElementById("picture").src = "images/thirtyone.jpg";
+        document.getElementById("caption").textContent = "It was really hot outside this summer so every time we got back from a walk Apollo hit this pose";
+        break;
+    case 32:
+        document.getElementById("picture").src = "images/thirtytwo.jpg";
+        document.getElementById("caption").textContent = "He loves sitting out in the backyard during the summer";
+        break;
+    case 33:
+        document.getElementById("picture").src = "images/thirtythree.jpg";
+        document.getElementById("caption").textContent = "Apollo getting a nice hug from my sister";
+        break;
+    case 34:
+        document.getElementById("picture").src = "images/thirtysix.jpg";
+        document.getElementById("caption").textContent = "When we cleaned out the basement a couple years ago, we sold a bunch of old toys on Facebbok Marketplace. The thing was that we had to take pictures of the stuff first. Apollo wasn't a huge fan.";
+        break;
+    case 35:
+        document.getElementById("picture").src = "images/thirtyseven.jpg";
+        document.getElementById("caption").textContent = "Apollo likes coming to the pumpkin patch in the fall";
+        break;
+    case 36:
+        document.getElementById("picture").src = "images/thirtyeight.jpg";
+        document.getElementById("caption").textContent = "He just woke up";
+        break;
+    case 37:
+        document.getElementById("picture").src = "images/thirtynine.jpg";
+        document.getElementById("caption").textContent = "He has to investigate all the pumpkins. Obviously.";
+        break;
+    case 38:
+        document.getElementById("picture").src = "images/forty.jpg";
+        document.getElementById("caption").textContent = "Apollo's 5th birthday! He was super patient while we took the picture but after that he snarfed it down quickly";
+        break;
+    case 39:
+        document.getElementById("picture").src = "images/fortyone.jpg";
+        document.getElementById("caption").textContent = "There are always huge snow piles at the elementary school by my house in the winter and Apollo and I love to climb all over them.";
+        break;
+    case 40:
+        document.getElementById("picture").src = "images/fortytwo.jpg";
+        document.getElementById("caption").textContent = "My grandma took this picture when she was taking care of him while we were away. He looks a lot more dignified than usual.";
+        break;
+    case 41:
+        document.getElementById("picture").src = "images/fortythree.jpg";
+        document.getElementById("caption").textContent = "We dressed him up as Buzz Lightyear for Halloween a couple of years ago. He was not a big fan of the hood, but he wore the rest of the costume all night.";
+        break;
+    case 42:
+        document.getElementById("picture").src = "images/fortyfour.jpg";
+        document.getElementById("caption").textContent = "What a fancy puppy";
+        break;
+    case 43:
+        document.getElementById("picture").src = "images/fortyfive.jpg";
+        document.getElementById("caption").textContent = "Snoozy boy";
+        break;
+    case 44:
+        document.getElementById("picture").src = "images/fortysix.jpg";
+        document.getElementById("caption").textContent = "A picture from one of his first days at home. He was so tiny!";
+        break;
+    case 45:
+        document.getElementById("picture").src = "images/fortyseven.jpg";
+        document.getElementById("caption").textContent = "When Apollo came home, he loved chasing every fallen leaf he saw. It tired him out fast";
+        break;
+    case 46:
+        document.getElementById("picture").src = "images/fortyeight.jpg";
+        document.getElementById("caption").textContent = "Canada Day puppy! It was really hot out but he came downtown and wore his bandana. It was a hit with the little kids.";
+        break;
+    case 47:
+        document.getElementById("picture").src = "images/fortynine.jpg";
+        document.getElementById("caption").textContent = "We got him a Baby Yoda toy but it was the same size as him.";
+        break;
+    case 48:
+        document.getElementById("picture").src = "images/fifty.jpg";
+        document.getElementById("caption").textContent = "Donut dog in a holiday sweater";
+        break;
+    case 49:
+        document.getElementById("picture").src = "images/fiftyone.jpg";
+        document.getElementById("caption").textContent = "The first time Apollo saw snow. He had a great time pouncing on the flakes as they fell and sticking his face in the powder.";
+        break;
+    case 50:
+        document.getElementById("picture").src = "images/fiftytwo.jpg";
+        document.getElementById("caption").textContent = "One of the pictures the breeder showed us when we got Apollo";
+        break;
+    case 51:
+        document.getElementById("picture").src = "images/fiftythree.jpg";
+        document.getElementById("caption").textContent = "He used to love sitting in our laps, but he couldn't jump high enough to reach the couch, so we sat on the floor and he hopped right in.";
+        break;
+    case 52:
+        document.getElementById("picture").src = "images/fiftyfour.jpg";
+        document.getElementById("caption").textContent = "These days Apollo is around the same size as Mom's flower pots.";
+        break;
+    case 53:
+        document.getElementById("picture").src = "images/fiftyfive.jpg";
+        document.getElementById("caption").textContent = "He loves going to the beach and rolling around in the seaweed";
+        break;
+    case 54:
+        document.getElementById("picture").src = "images/fiftysix.jpg";
+        document.getElementById("caption").textContent = "Mom holding Apollo when we took him home for the first time";
+        break;
+}
+}
+generateImage();
+
+
+    
